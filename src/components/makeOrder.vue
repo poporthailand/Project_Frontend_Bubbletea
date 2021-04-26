22c2261dfc1d6cd7347ae549cc3e79bb50dd2261
poporthailand <ismat_rr@hotmail.com>
2021-04-26 22:57:23 +0700

    save

--- src/components/makeOrder.vue
+++ src/components/makeOrder.vue
@@ -313,7 +313,6 @@ export default {
     handleSubmitForm(){
       //console.log('++');
         let r = confirm('ต้องการทำรายการนี้ ?')
-        console.log(this.product[i].name)
         if (r){
       for(let i = 0; i < this.product.length; i++){
         if (this.menu == this.product[i].name){
