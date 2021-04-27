df0a97122a156784ed2b49cb755c1a107aa9ea15
poporthailand <ismat_rr@hotmail.com>
2021-04-28 05:32:19 +0700

    save

--- src/components/homePage.vue
+++ src/components/homePage.vue
@@ -1,8 +1,8 @@
 <template>
     <center>
  <div class="ctn">
-      <div class="header">
-        <img src="../assets/img/main/banner3.png">
+      <div class="header" style="backgroundImage: url(https://1.bp.blogspot.com/-AXFiL-E39GE/YIiPv1Gbn6I/AAAAAAAAABA/BJKo1aDf_hMwFP0xdQpUl0WUpvFcgk_BQCLcBGAsYHQ/s1920/banner3.png) ">
+        
       </div>
       <div class="Body" style=" backgroundImage: url(https://1.bp.blogspot.com/-TBd-qil9b3U/YIiKQwttW6I/AAAAAAAAAA4/GPLWSC2ADZ86bXo5y1IFR3MiKEjd23G9wCLcBGAsYHQ/s1920/bgcenter.png) ">
         <div class="bodyleft" >
@@ -24,8 +24,8 @@
         </div>
         
       </div>
-      <div class="footer">
-        <img src="../assets/img/main/footage.png">
+      <div class="footer" style="backgroundImage: url(https://1.bp.blogspot.com/-oV-TpJB_jLY/YIiP9dchMaI/AAAAAAAAABE/o4blpwJyYC0VMv0mA0O0HosDsN7vKds2ACLcBGAsYHQ/s1920/footage.png) ">
+        
       </div>
     </div>
   </center>
