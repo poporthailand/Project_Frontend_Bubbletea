f67855a5dc4bb4a847e45f6f7c9e4d4af64bb7a7
poporthailand <ismat_rr@hotmail.com>
2021-04-28 05:18:41 +0700

    save

--- src/components/homePage.vue
+++ src/components/homePage.vue
@@ -2,29 +2,31 @@
     <center>
  <div class="ctn">
       <div class="header">
-        
+        <img src="../assets/img/main/banner3.png">
       </div>
-      <div class="Body" style=" backgroundImage: url(https://1.bp.blogspot.com/-LySYqXBTJ3E/YH7y54hxTvI/AAAAAAAAAAk/aSJx4u79H_4ip60o25mujuP0mY90Zgr2wCLcBGAsYHQ/s1920/3.png) ">
+      <div class="Body" style=" backgroundImage: url(https://1.bp.blogspot.com/-TBd-qil9b3U/YIiKQwttW6I/AAAAAAAAAA4/GPLWSC2ADZ86bXo5y1IFR3MiKEjd23G9wCLcBGAsYHQ/s1920/bgcenter.png) ">
         <div class="bodyleft" >
           <center>
-      <img src="../assets/img/main/1.png" style="margin-top: 90px;">
+      
           </center>
         </div>
         <div class="bodycenter" align=center>
-          <router-link to="/makeOrder"><button class="bodybutton" style="background-color:#F79B8F; margin-top:140px;">Make order</button></router-link>
+          <router-link to="/makeOrder"><button class="bodybutton1" style="margin-top:140px;">Make order</button></router-link>
           <br>
-          <router-link to="/checkStock"><button class="bodybutton" style="background-color:#F7B4A8;margin-top:50px;">Check stock</button></router-link>
+          <router-link to="/checkStock"><button class="bodybutton2" style="margin-top:50px;">Check stock</button></router-link>
           <br>
-          <router-link to="/historySale"><button class="bodybutton" style="background-color:#FAE4E1;margin-top:50px;">History sale</button></router-link>
+          <router-link to="/historySale"><button class="bodybutton3" style="margin-top:50px;">History sale</button></router-link>
         </div>
         <div class="bodyright">
           <center>
-          <img src="../assets/img/main/2.png" style="margin-top: 90px;">
+          
         </center>
         </div>
         
       </div>
-      <div class="footer"></div>
+      <div class="footer">
+        <img src="../assets/img/main/footage.png">
+      </div>
     </div>
   </center>
   
@@ -41,18 +43,14 @@ export default {
 </script>
 
 <style scoped>
-button:hover {
-  opacity: 0.5;
-}
+
 .ctn {
     background-color: rgb(252, 232, 206);
     width: 100%;
     height: 969px;
     
   }
-  .header:hover {
-    opacity: 0.8;
-  }
+  
   .header {
     background-color: #faa9ba;
     width: 100%;
@@ -67,9 +65,7 @@ button:hover {
     width: 100%;
     height: 100px;
   }
-  .footer:hover {
-    opacity: 0.8;
-  }
+  
   .bodyleft {
     width: 33.3%;
     height: 100%;
@@ -86,12 +82,38 @@ button:hover {
     float: left;
   }
   
-  .bodybutton {
+  .bodybutton1 {
     color: aliceblue;
     padding: 25px 30px;
     font-size: 50px;
     border-radius: 45px;
     border: 3px solid #000;
+    background-color:#704d2d;
+  }
+  .bodybutton1:hover{
+    background-color: #3a2817;
   }
   
+  .bodybutton2 {
+    color: aliceblue;
+    padding: 25px 30px;
+    font-size: 50px;
+    border-radius: 45px;
+    border: 3px solid #000;
+    background-color:#84644b;
+  }
+  .bodybutton2:hover{
+    background-color: #584332;
+  }
+  .bodybutton3 {
+    color: aliceblue;
+    padding: 25px 30px;
+    font-size: 50px;
+    border-radius: 45px;
+    border: 3px solid #000;
+    background-color:#997e6d;
+  }
+  .bodybutton3:hover{
+    background-color: #68564a;
+  }
 </style>
